6db726b7e8db623b8d67ee2b13de9a51c9286bdb
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2025-09-19 15:44:18 +0530

    Scraped all module ids

--- src/getAllModuleId.ts
+++ src/getAllModuleId.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 const someId = "d466e3e4-1f96-4802-b0cc-eaa5b01e928d"; // Could be some sort of userId but don't know
 
 export async function getModuleId() {
+    // TODO: What is this other id?
     const response = await fetch(`https://api.kodnest.in/assessment-service/api/v2/consumers/practices/all/${someId}/a86dde74-70f1-4e17-a3fd-a48502859c87`, {
         "headers": {
             "accept": "application/json, text/plain, */*",
